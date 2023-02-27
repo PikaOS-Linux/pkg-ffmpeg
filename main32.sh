7e36d6d6c92ff60edcc663b7f6de3ad2ab251132
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-27 18:00:17 +0300

    first commit

---
+++ main32.sh
@@ -0,0 +1,27 @@
+# Add dependent repositories
+sudo dpkg --add-architecture i386
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+
+# Clone Upstream
+git clone https://github.com/FFmpeg/FFmpeg -b release/6.0
+cp -rvf ./debian ./FFmpeg
+mv ./FFmpeg ./ffmpeg
+cd ./ffmpeg
+
+# Get build deps
+apt-get install build-essential -y
+apt-get install crossbuild-essential-i386 lib32gcc-11-dev -y
+apt-get build-dep ./ -y -a i386
+apt-get install -y cleancss doxygen node-less tree
+
+# Build package
+LOGNAME=root dh_make --createorig -y -l -p ffmpeg_6.0.0
+dpkg-buildpackage -a i386
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
