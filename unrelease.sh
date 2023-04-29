611e9353082728ea1398d43baa7db4e2bcb7b756
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-04-30 00:09:39 +0300

    Luuuuuuuuuuuuuuuunaaaaaaaaaaar

--- unrelease.sh
+++ unrelease.sh
@@ -3,15 +3,15 @@ mkdir -p ./output/repo
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% ffmpeg*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavcodec60*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavdevice60*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavfilter9*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavformat60*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libavutil58*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libpostproc57*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libswresample4*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% libswscale7*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% ffmpeg*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavcodec60*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavdevice60*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavfilter9*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavformat60*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libavutil58*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libpostproc57*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libswresample4*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libswscale7*)'
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
